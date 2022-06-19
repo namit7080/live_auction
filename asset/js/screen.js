b1e821a4787d0a701c74b9317eb1143d6ab3985e
namit7080 <81043272+namit7080@users.noreply.github.com>
2022-06-19 18:37:52 +0530

    Update screen.js

--- asset/js/screen.js
+++ asset/js/screen.js
@@ -1,14 +1,14 @@
 var b = window.innerWidth;
 var a = window.innerHeight;
 console.log(a+" &&"+b);
-if(a<650||b<1100){
+if(a<400||b<600){
     window.location = "/blocked";
  }
 window.addEventListener('resize', function(){
     a = window.screen.height;
     b = window.screen.width;
     console.log(a+" "+b);
-    if(a<650||b<1100){
+    if(a<400||b<600){
        window.location = "/blocked";
     }
    
